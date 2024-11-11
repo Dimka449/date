50e0b11d2e0f477f7330aab37eda1dc113650743
пк <negulyaevd65@gmail.com>
2024-11-11 19:13:17 +0400

    issue

---
+++ dates.py
@@ -0,0 +1,16 @@
+import datetime
+from array import array
+dates = array("i",[3])
+cd = datetime.date.today()
+current_weekday = cd.weekday()
+current_date = datetime.date.today().isoformat()
+dates = current_date.split("-")
+year = dates[0]
+month = dates[1]
+date = dates[2]
+if(current_weekday == 5 or current_weekday == 6):
+    print("выходной")
+elif(month == "01" and date == "01"):
+    print("выходной")
+elif(month == "09" and date == "02"):
+    print("выходной")
