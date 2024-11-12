2ebe9416e95e0a0bb591e2081d597f4f390bc030
пк <negulyaevd65@gmail.com>
2024-11-12 21:24:39 +0400

    issue

--- dates.py
+++ dates.py
@@ -1,17 +1,18 @@
-import datetime
+# import datetime
+from datetime import date
 
-cd = datetime.date.today()
+cd = date.today()
 current_weekday = cd.weekday()
-current_date = datetime.date.today().isoformat()
+current_date = date.today().isoformat()
 dates = current_date.split("-")
 year = dates[0]
 month = dates[1]
-date = dates[2]
+day = dates[2]
 if current_weekday == 5 or current_weekday == 6:
     print("выходной")
-elif month == "01" and date == "01":
+elif month == "01" and day == "01":
     print("выходной")
-elif month == "09" and date == "21":
+elif month == "09" and day == "21":
     print("выходной")
 else:
     print("всё ещё рабочий")
